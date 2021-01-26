1db27506b88a17dd3db8c7595544295b2fab953b
Alessandro Liggieri <smmb@live.it>
2021-01-26 15:45:12 +0100

    Esercizio completo

---
+++ js/script.js
@@ -0,0 +1,8 @@
+var app = new Vue({
+ el: '#root',
+ data: {
+   titleContent: 'Metal Gear Solid 3',
+   imgContent: 'https://www.spaziogames.it/wp-content/uploads/2015/12/metal-gear-solid-3.jpg'
+ }
+});
+Vue.config.devtools = true
